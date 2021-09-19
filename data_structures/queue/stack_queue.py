dfefbad7d2409fe9f7fe7a93573d024c1bb0e01f
Jhih-Yu Chen <a402250025@gmail.com>
2021-09-19 23:56:24 +0800

    add queue & stack-based queue

--- data_structures/queue/stack_queue.py
+++ data_structures/queue/stack_queue.py
@@ -12,17 +12,3 @@ class Stack_Queue:
             while self.input_stack:
                 self.output_stack.append(self.input_stack.pop())
         return self.output_stack.pop()
-
-
-
-if __name__ == '__main__':
-    test_queue = Stack_Queue()
-
-    import time
-    start_time = time.time()
-    for i in range(100000):
-        #print(i)
-        test_queue.enqueue(i)
-    for i in range(100000):
-        test_queue.dequeue()
-    print("--- %s seconds ---" % (time.time() - start_time))
