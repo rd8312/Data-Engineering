ed586385ecd7788701b56d28161a1397cae95a1b
Jhih-Yu Chen <a402250025@gmail.com>
2022-03-02 20:14:14 +0800

    add hello world!

---
+++ programming/C_language/hello_world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+*/
+
+int main(int argc, char *argv[])
+{
+    printf("Hello World!你好 c語言\n");
+    //printf("Hello World!你好 c語言\n");
+    printf("Hello World!你好 c語言\n");
+
+    system("pause"); 
+    return 0;
+}
