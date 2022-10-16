974c8ab587428e75a2625841a1934bf9b24b08a5
Jhih-Yu Chen <a402250025@gmail.com>
2022-10-16 14:38:21 +0800

    add adjacency list

--- data_structures/graph/Adjacency_list.py
+++ data_structures/graph/Adjacency_list.py
@@ -1,8 +1,10 @@
-# using dictionary can directly use the vertex labels
+# using list implemented adjacency matrix
+graph = [['B', 'C'], ['E', 'A'], ['A', 'B', 'E', 'F'], ['B', 'C'], ['C']]
 
-graph = {}
-graph['A'] = ['B', 'C']
-graph['B'] = ['E', 'A']
-graph['C'] = ['A', 'B', 'E', 'F']
-graph['E'] = ['B', 'C']
-graph['F'] = ['C']  
+# using dictionary can directly use the vertex labels
+# graph = {}
+# graph['A'] = ['B', 'C']
+# graph['B'] = ['E', 'A']
+# graph['C'] = ['A', 'B', 'E', 'F']
+# graph['E'] = ['B', 'C']
+# graph['F'] = ['C']  
