66e30edd451c8c6b1de5a5a2689208cca6222176
Jhih-Yu Chen <a402250025@gmail.com>
2022-10-21 22:22:39 +0800

    add utils

--- algorithms/graph/utils.py
+++ algorithms/graph/utils.py
@@ -1,17 +1,13 @@
-from datetime import date
-
-
 class AbstractTree:
     def __init__(self, data):
-        self.data = data
-        self.info = {}
+        self.data = { "data" : data}
 
 class Leaf(AbstractTree):
-    def __init__(self, data):
+    def __init__(self, data) -> AbstractTree:
         super().__init__(data)
 
 class BiTree(AbstractTree):
-    def __init__(self, data):
+    def __init__(self, data) -> AbstractTree:
         super().__init__(data)
         self.left_child = None
         self.right_child = None
