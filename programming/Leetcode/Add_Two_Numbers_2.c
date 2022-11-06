544f97be663d65764b725e477319c7c09aed4d89
Jhih-Yu Chen <a402250025@gmail.com>
2022-11-06 21:11:50 +0800

    add leetcode 2

---
+++ programming/Leetcode/Add_Two_Numbers_2.c
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode* result, *tmp, *prev;
+    int carry = 0; int count = 0;
+  while(l1 || l2 || carry) {
+    int val1 = 0, val2 = 0, val3 = 0;
+    if (l1) {
+      val1 = l1->val;
+      l1 = l1->next;
+    }
+    if (l2) {
+      val2 = l2->val;
+      l2 = l2->next;
+    }
+    val3 = val1 + val2 + carry;
+    carry = val3 / 10;
+    tmp = (struct ListNode *) malloc(sizeof(struct ListNode));
+    tmp->val = val3 % 10; 
+    tmp->next = NULL;
+    
+    if (!count)
+      result = tmp;
+    else
+      prev->next = tmp;
+    
+    prev = tmp;
+    count++;
+  }
+  return result;
+}
--- programming/Leetcode/Add_Two_Numbers_2.py
+++ programming/Leetcode/Add_Two_Numbers_2.py
@@ -1,3 +1,8 @@
+"""
+time complexity = O(n)
+space complexity = O(n)
+"""
+
 # Definition for singly-linked list.
 class ListNode:
    def __init__(self, val=0, next=None):
