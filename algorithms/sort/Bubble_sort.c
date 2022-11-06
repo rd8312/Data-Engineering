b0ba035663c61fca6e7b96078d616d952458de9e
Jhih-Yu Chen <a402250025@gmail.com>
2022-11-06 22:47:50 +0800

    add bubble sort

--- algorithms/sort/Bubble_sort.c
+++ algorithms/sort/Bubble_sort.c
@@ -24,8 +24,56 @@ void bubbleSort(int arr[], int n)
     return 0;
 }
 
+/*
+// implement bubble sort by linked list
+struct Node
+{
+    int val;
+    struct Node *next;
+};
+    
+void swap(struct Node* node_1, struct Node* node_2)
+{
+    int temp = node_1->val;
+    node_1->val = node_2->val;
+    node_2->val = temp;
+}
+
+void bubbleSort(struct Node *head)
+{
+    int swapped;
+    struct Node *ptr1;
+    struct Node *lptr = NULL; // avoid circular linked list
+
+    if (head == NULL)
+    {
+        return;
+    }
+
+    do
+    {
+        swapped = 0;
+        ptr1 = head;
+
+        while (ptr1->next != lptr)
+        {
+            if (ptr1->val > ptr1->next->val)
+            {
+                swap(ptr1, ptr1->next);
+                swapped = 1;
+            }
+
+            ptr1 = ptr1->next;
+        }
+
+        lptr = ptr1;
+    } while (swapped);
+}
+*/
+
 int main()
 {
+    
     int ary[5] = {10, 5, 21, 9 ,11};
     int n;
     bubbleSort(ary, 5);
