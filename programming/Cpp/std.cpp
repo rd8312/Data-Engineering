e28ec368786724955c7299b5480f584969e0963b
Jhih-Yu Chen <a402250025@gmail.com>
2024-10-04 02:21:42 +0800

    feat: add std

---
+++ programming/Cpp/std.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/*
+<iostream>內的名稱會放置在 std 命名空間內
+std 是 c++ 標準庫所使用的命名空間名稱 
+*/
+
+using namespace std;
+
+int main(){
+	int cout = 666;
+	std::cout << cout << endl;
+	return 0;
+}
+
