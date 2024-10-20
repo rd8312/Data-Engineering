5fbb690f6ee363fc229e7d8fde884b79e0ae9a6d
Jhih-Yu Chen <a402250025@gmail.com>
2024-10-21 01:12:49 +0800

    feat:update assignment

--- programming/Cpp/assignment.cpp
+++ programming/Cpp/assignment.cpp
@@ -15,6 +15,24 @@ int main(){
 	cout << "c: " << c << endl
 		 << "d: " << d << endl
 	     << "e: " << e << endl;
+	     
+	int f = 3;
+	int g;
+	g = f = f + 1;
+	cout << "f: " << f << endl
+	     << "g: " << g << endl;
+	
+	int h = 3;
+	int i;
+	i = ++h; // ++h -> h = h + 1
+	cout << "h: " << h << endl
+	     << "i: " << i << endl;
+	     
+	int j = 3;
+	int k;
+	k = j++; // j++ -> j = j + 1
+	cout << "j: " << j << endl
+	     << "k: " << k << endl;
 	
 	return 0;
 }
